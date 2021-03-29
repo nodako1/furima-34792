70bbfd5e09959c22c9346a09ed30191384d35b2e
nodako1 <79571927+nodako1@users.noreply.github.com>
2021-03-29 15:07:52 +0900

    items_controllerの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,25 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
+    @items = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :category_id, :status_id, :shipping_cost_id, :state_id, :days_to_delivery_id).merge(user_id: current_user.id)
   end
 end
