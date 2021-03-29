412841c529d69743be25a243e423a2f67ddf770a
nodako1 <79571927+nodako1@users.noreply.github.com>
2021-03-29 12:47:03 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,14 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
