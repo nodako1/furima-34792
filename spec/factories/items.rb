7df0d3ecc5c998f6fbe72dceea4ab0bcf237d3c0
nodako1 <79571927+nodako1@users.noreply.github.com>
2021-03-29 16:10:02 +0900

    factorybot/itemsを設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
   factory :item do
+    name                { 'もち' }
+    description         { '説明' }
+    category_id         { '2' }
+    status_id           { '2' }
+    shipping_cost_id    { '2' }
+    state_id            { '2' }
+    days_to_delivery_id { '2' }
+    price               { '2000' }
   end
 end
