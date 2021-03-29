0761ee14f78e59cf0576396444193cb03b62fe9a
nodako1 <79571927+nodako1@users.noreply.github.com>
2021-03-29 15:02:10 +0900

    ActiveHash::Baseの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,15 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :purchase
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :state, :shipping_cost, :days_to_delivery
+
+  with_options presence: true do
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :state_id, numericality: { other_than: 1 }
+  validates :shipping_cost_id, numericality: { other_than: 1 }
+  validates :days_to_delivery_id, numericality: { other_than: 1 }
+  end
 end
