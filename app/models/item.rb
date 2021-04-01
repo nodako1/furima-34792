fae89b96b3784ba4917fd7df94598c4cd0ed544c
nodako1 <79571927+nodako1@users.noreply.github.com>
2021-04-01 16:24:36 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
     validates :name
     validates :description
     validates :price, format: { with: /\A[0-9]+\z/ },
-                      numericality: { only_integer: true, greater_than: 300, less_than: 10_000_000 }
+                      numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
     with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :status_id
