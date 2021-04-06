bdc8a9ace75f98b38bc46e185e6b4058883c368d
nodako1 <79571927+nodako1@users.noreply.github.com>
2021-04-06 23:40:16 +0900

    マイグレーションの作成

---
+++ db/migrate/20210405030005_create_shipments.rb
@@ -0,0 +1,14 @@
+class CreateShipments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shipments do |t|
+      t.string     :post_code,         null: false
+      t.integer    :state_id,          null: false
+      t.string     :city,              null: false
+      t.string     :street_address,    null: false
+      t.string     :suburb
+      t.string     :telephone,         null: false
+      t.references :purchase,          null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
